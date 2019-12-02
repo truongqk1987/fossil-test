fdd72e2cd451239e58104239315fb5ca9be12be8
Khai Truong <KTruong@absolute.com>
2019-12-02 15:00:31 +0700

    Apply scroll to top after click Menu item

--- src/pages/Order/StatusSidebar/Status/Status.js
+++ src/pages/Order/StatusSidebar/Status/Status.js
@@ -14,17 +14,20 @@ const stylesheet = theme => ({
             padding: '1rem',
             textAlign: 'center',
             width: '100%',
+            '&.active': {
+                color: theme.pages.order.sidebarActiveItemTextColor,
+                background: theme.pages.order.sidebarActiveItemBackground,
+            }
         },
-        '& .active': {
-            color: theme.pages.order.sidebarActiveItemTextColor,
-            background: theme.pages.order.sidebarActiveItemBackground,
-        }
+        
     }
 })
 
 const Status = ({ classes, active, url, label }) => 
-    <li className={classes.Status}>
-        <NavLink exact to={url} activeClassName="active">{label}</NavLink>
+    <li className={classes.Status} onClick={() => { window.scrollTo(0, 0) }}>
+        <NavLink exact to={url} activeClassName="active">
+            {label}
+        </NavLink>
     </li>
 
 export default injectSheet(stylesheet)(Status);
